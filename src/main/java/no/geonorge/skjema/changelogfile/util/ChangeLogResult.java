1ac44f3e653982a265c198909ffca7f8b80781a7
Lars Opsahl <lop@skogoglandskap.no>
2013-10-25 06:42:45 +0200

    Used for holding results after unmarshal

---
+++ src/main/java/no/geonorge/skjema/changelogfile/util/ChangeLogResult.java
@@ -0,0 +1,13 @@
+package no.geonorge.skjema.changelogfile.util;
+
+import java.util.ArrayList;
+
+public class ChangeLogResult {
+
+	public ArrayList<Object> insertTypetList = new ArrayList<>();
+	
+	public ArrayList<Object> updateTypeList = new ArrayList<>();
+	
+	public ArrayList<Object> deleteTypeList = new ArrayList<>();
+
+}

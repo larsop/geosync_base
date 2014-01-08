de21353a4d10ee50c2ebe00db38108236d0e1162
Lars Opsahl <lop@pc359.ad.skogoglandskap.no>
2014-01-08 14:48:45 +0100

    more tests union of linestrings

--- src/test/java/itest/no/skogoglanskap/provider/Arealressurs/TestGenerateInsertChangelogFile.java
+++ src/test/java/itest/no/skogoglanskap/provider/Arealressurs/TestGenerateInsertChangelogFile.java
@@ -7,12 +7,15 @@ package itest.no.skogoglanskap.provider.Arealressurs;
  *  
  */
 
+
+// 
 import java.math.BigInteger;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Iterator;
 
 import javax.xml.bind.JAXBElement;
@@ -43,7 +46,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.CoordinateArrays;
+import com.vividsolutions.jts.geom.CoordinateList;
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.CoordinateSequenceFactory;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.LinearRing;
@@ -52,6 +59,7 @@ import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
+import com.vividsolutions.jts.operation.linemerge.LineMerger;
 import com.vividsolutions.jts.operation.overlay.snap.GeometrySnapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -72,6 +80,8 @@ public class TestGenerateInsertChangelogFile {
 	@Test
 	public void testMappingOfAr5FlateProvSimpleFeatureEntity() throws Exception {
 
+		
+		
 		// add 2 polygons with a commmon border
 		ArrayList<Ar5FlateProvSimpleFeatureEntity> providerData = new ArrayList<>();
 		providerData.add(addPolygonOne());
@@ -81,7 +91,6 @@ public class TestGenerateInsertChangelogFile {
 		ArrayList<ArealressursFlateType> subscriberSurfcaeData = new ArrayList<>();
 		// this the list of common border objects
 		ArrayList<ArealressursGrenseType> subscriberBorderData = new ArrayList<>();
-		ArrayList<LineString> lineStringsOrg = new ArrayList<LineString>();
 
 		Assert.assertTrue("To few rows created " + providerData.size(), providerData.size() > 0);
 
@@ -89,91 +98,265 @@ public class TestGenerateInsertChangelogFile {
 
 		GeometryFactory gf = new GeometryFactory();
 
+
+		GeometrySnapper geometrySnapper = null;
+		
+		double computeOverlaySnapTolerance = 0.00001;
+
+
+		//LineMerger lm = new LineMerger();
+		ArrayList<LineString> lineStringsOrg = new ArrayList<LineString>();
 		// find all line strings in all geometries
 		for (Ar5FlateProvSimpleFeatureEntity aa : providerData) {
 
 			Polygon p = (Polygon) aa.getGeo();
 
 			LineString lineString = getLineString(p.getExteriorRing(), gf);
+			
+//			// test add single linestrings
+//			Coordinate[] coordinates = lineString.getCoordinates();
+//			for (int i = 0; i < coordinates.length - 1; i++) {
+//	
+//				Coordinate[] cp = { coordinates[i], coordinates[i + 1] };
+//				LineString createLineString = gf.createLineString(cp);
+//				lineStringsOrg.add(createLineString);
+//				
+//			}
+
+
 			lineStringsOrg.add(lineString);
 
-			for (int i = 0; i < p.getNumInteriorRing(); i++) {
-				LineString ls = getLineString(p.getInteriorRingN(i), gf);
-				lineStringsOrg.add(ls);
-			}
+			
+
+//			for (int i = 0; i < p.getNumInteriorRing(); i++) {
+//				LineString ls = getLineString(p.getInteriorRingN(i), gf);
+//				Geometry[] snap = geometrySnapper.snap(geometryCollectionOrgMultiLineString, ls, computeOverlaySnapTolerance);
+//				geometryCollectionOrgMultiLineString = snap[0].union(snap[1]);
+//				
+//			}
 
 		}
+
+		
+		for (int i = 0; i < lineStringsOrg.size(); i++) {
+			Geometry g = lineStringsOrg.get(i);
+			if (g instanceof LineString) {
+				System.out.println("Test Used " + g.getUserData() + ":" + g.toText());
+			} else {
+				System.out.println("Test Not used " + g.getUserData() + ":" + g.toText());
+			}
+		}
+
 
 		// find common line string
-		LineString[] lineStringsTable = lineStringsOrg.toArray(new LineString[lineStringsOrg.size()]);
+		// Create a collection of org linestrings
+		MultiLineString geometryCollectionOrgMultiLineString = gf.createMultiLineString(lineStringsOrg.toArray(new LineString[lineStringsOrg.size()]));
+//		
+//		// Get a union of all of the line strings  
+		Geometry newLinstringGeo = geometryCollectionOrgMultiLineString.union();
 
-		MultiLineString createMultiLineString = gf.createMultiLineString(lineStringsTable);
 
-		Geometry commonLinsStringList = createMultiLineString.union();
-
-		for (int i = 0; i < commonLinsStringList.getNumGeometries(); i++) {
-			Geometry g = commonLinsStringList.getGeometryN(i);
-			System.out.println(g.getUserData() + ":" + g.toText());
+		// hold the list of linstrings after union
+		ArrayList<LineString> lineStringsNew = new ArrayList<LineString>();
+		// make a collection of only line strings, remove points and other geos
+		for (int i = 0; i < newLinstringGeo.getNumGeometries(); i++) {
+			Geometry g = newLinstringGeo.getGeometryN(i);
+			if (g instanceof LineString) {
+				g.setUserData(g.hashCode());
+				lineStringsNew.add((LineString) g);
+				System.out.println("Used " + g.getUserData() + ":" + g.toText());
+			} else {
+				System.out.println("Not used " + g.getUserData() + ":" + g.toText());
+			}
 		}
+		
+//		Collection mergedLineStrings = lm.getMergedLineStrings();
+//		for (Object a : mergedLineStrings) {
+//			Geometry g = (Geometry) a;
+//			if (g instanceof LineString) {
+//				g.setUserData(g.hashCode());
+//				lineStringsNew.add((LineString) g);
+//				System.out.println("Used " + g.getUserData() + ":" + g.toText());
+//			} else {
+//				System.out.println("Not used " + g.getUserData() + ":" + g.toText());
+//			}
+//			
+//		}
+		
+		
+		// craete a collection of new linsetrings
+		MultiLineString geometryCollectionNewMultiLineString = gf.createMultiLineString(lineStringsNew.toArray(new LineString[lineStringsNew.size()]));
+
 
 
 		//GeometrySnapper geometrySnapper = new GeometrySnapper(commonLinsStringList);
 
 		// replace the borders with the new linstrings
 		for (Ar5FlateProvSimpleFeatureEntity aa : providerData) {
-			// find all line strings in all geometries
-			Polygon p = (Polygon) aa.getGeo();
-			LineString lineString = getLineString(p.getExteriorRing(), gf);
 
-			LinearRing shell = findCommonLineString(gf, commonLinsStringList, lineString);
+
+			
+			Polygon p = (Polygon) aa.getGeo();
+
+			// get exterior ring
+			LineString thisLineString = getLineString(p.getExteriorRing(), gf);
+
+			// hold the list linstrings that is of interest for this polygon 
+			ArrayList<LineString> lineStringstmp = new ArrayList<LineString>();
+
+			// get all that touch touches this polygon
+			for (LineString ls : lineStringsNew) {
+				if (thisLineString.intersects(ls) && thisLineString.intersection(ls).getLength() > 0.00000)  {
+					lineStringstmp.add(ls);
+				}
+			}
+			
+			
+			
+
+			
+			LinearRing shell = findCommonLineString(gf, lineStringstmp, thisLineString);
 			
 			LinearRing[] holes = new LinearRing[p.getNumInteriorRing()];
-			for (int i = 0; i < p.getNumInteriorRing(); i++) {
-				LineString ls = getLineString(p.getInteriorRingN(i), gf);
-				LinearRing h = findCommonLineString(gf, commonLinsStringList, ls);
-				holes[i] = h;
-			}
+			
+//			for (int i = 0; i < p.getNumInteriorRing(); i++) {
+//				LineString ls = getLineString(p.getInteriorRingN(i), gf);
+//				LinearRing h = findCommonLineString(gf, commonLinsStringGeometry, ls);
+//				holes[i] = h;
+//			}
 
-			Geometry newtopoGeometry = gf.createPolygon(shell);
+			Geometry newtopoGeometry = gf.createPolygon(shell, holes);
 			
 			aa.setGeo(newtopoGeometry);
-			//System.out.println(newtopoGeometry.toText());
+			System.out.println(newtopoGeometry.toText());
 		}
 		
 		// we a list common linstrings and they are used  
 
 	}
 
-	private LinearRing findCommonLineString(GeometryFactory gf, Geometry commonLinsStringList, LineString lineString) {
-		ArrayList<Coordinate> coordinatesNewList = new ArrayList<>();
+	@Test
+	public void testSimpleLinestringUnion() throws Exception {
 
-		Coordinate[] coordinates = lineString.getCoordinates();
-		Coordinate lastOkPoint = coordinates[0];
+		// two overlapping lines 
+		WKTReader reader = new WKTReader();
 
-		for (int i = 0; i < coordinates.length - 1; i++) {
 
-			Coordinate[] cp = { lastOkPoint, coordinates[i + 1] };
-			LineString createLineString = gf.createLineString(cp);
+//		LINESTRING (59.310381 4.90523, 59.310381 4.905582)
+//		LINESTRING (59.310381 4.905582, 59.310381533333334 4.905355308333333, 59.310381 4.90523)
+//		LineString l1 = (LineString) reader.read("LINESTRING (59.310381 4.90523, 59.310381 4.905582)");
+//		LineString l2  = (LineString) reader.read("LINESTRING (59.310381 4.905582, 59.310381533333334 4.905355308333333, 59.310381 4.90523)");
 
-			// TODO fins a better way to this
-			// get the intersection from the list of common ponts
-			Geometry intersection = commonLinsStringList.intersection(createLineString);
-			//if (commonLinsStringList.contains(createLineString)) {
-			if (intersection.getLength() == createLineString.getLength()) {
-			//if (intersection.equalsTopo(createLineString)) {
-				coordinatesNewList.add(lastOkPoint);
-				lastOkPoint = coordinates[i + 1];
-				
-			}
+		
+		LineString l1 = (LineString) reader.read("LINESTRING (59.310381 4.90523, 59.310381 4.905582)");
+		LineString l2  = (LineString) reader.read("LINESTRING (59.310381 4.90523, 59.310381533333334 4.905355308333333, 59.310381 4.905582)");
 
+		System.out.println("Hashcode before :" + l1.hashCode() + ":" + l2.hashCode());
+
+		GeometrySnapper geometrySnapper = new GeometrySnapper(l1);
+		
+//		double computeOverlaySnapTolerance = 0.000001;
+//		gir 
+//		1.0E-6:LINESTRING (59.310381 4.90523, 59.310381 4.905582)
+//		1.0E-6:LINESTRING (59.310381 4.90523, 59.310381 4.905582, 59.310381 4.905583)
+
+//		double computeOverlaySnapTolerance = 0.00001;
+//		gir 
+//		1.0E-5:LINESTRING (59.310381 4.90523, 59.310381 4.905583)
+//		1.0E-5:LINESTRING (59.310381 4.90523, 59.310381 4.905583)
+		
+		double computeOverlaySnapTolerance = 0.00001;
+		
+		Geometry[] snap = geometrySnapper.snap(l1, l2, computeOverlaySnapTolerance);
+		for (Geometry geometry : snap) {
+			System.out.println(computeOverlaySnapTolerance + ":" + geometry.toText());
 		}
+		
+		l1 = (LineString) snap[0];
+		l2 = (LineString) snap[1];
+		
+		System.out.println("Hashcode after :" + l1.hashCode() + ":" + l2.hashCode());
+		
+		
+		
+		System.out.println(l1.union(l2));
 
-		// add first point to close polygon
-		coordinatesNewList.add(coordinatesNewList.get(0));
-		Coordinate[] coordinatesNewListArray = coordinatesNewList.toArray(new Coordinate[coordinatesNewList.size()]);
 
-		LinearRing shell = gf.createLinearRing(coordinatesNewListArray);
-		return shell;
+		
+		
+		
+		System.out.println(l1.equalsTopo(l2));
+
+		
+
+	}
+	/** 
+	 * Replace the linerstring with linerstring from commonLinsStringList
+	 * @param gf
+	 * @param lineStringsListToUse
+	 * @param lineString
+	 * @return
+	 */
+	private LinearRing findCommonLineString(GeometryFactory gf, ArrayList<LineString> lineStringsListToUse, LineString lineString) {
+		
+//		ArrayList<LineString> resultLinsStringList = new ArrayList<>();
+//
+//		Coordinate[] coordinates = lineString.getCoordinates();
+//		Coordinate lastOkPoint = coordinates[0];
+//
+//		for (int i = 0; i < coordinates.length - 1; i++) {
+//
+//			Coordinate[] cp = { lastOkPoint, coordinates[i + 1] };
+//			LineString createLineString = gf.createLineString(cp);
+//			
+//			
+//			for (LineString orgLs : lineStringsListToUse) {
+//				
+//			}
+//
+//
+//			// TODO fins a better way to this
+//			// get the intersection from the list of common ponts
+//			Geometry intersection = lineStringsListToUse.intersection(createLineString);
+//			//if (commonLinsStringList.contains(createLineString)) {
+//			if (intersection.getLength() == createLineString.getLength()) {
+//			//if (intersection.equalsTopo(createLineString)) {
+//				coordinatesNewList.add(lastOkPoint);
+//				lastOkPoint = coordinates[i + 1];
+//				
+//			}
+//
+//		}
+//		
+//		for (int zi = 0; zi < coordinates.length - 1; zi++) {
+//
+//			Coordinate[] cp = { lastOkPoint, coordinates[zi + 1] };
+//			LineString createLineString = gf.createLineString(cp);
+//
+//			// TODO fins a better way to this
+//			// get the intersection from the list of common ponts
+//			Geometry intersection = lineStringsListToUse.intersection(createLineString);
+//			//if (commonLinsStringList.contains(createLineString)) {
+//			if (intersection.getLength() == createLineString.getLength()) {
+//			//if (intersection.equalsTopo(createLineString)) {
+//				coordinatesNewList.add(lastOkPoint);
+//				lastOkPoint = coordinates[zi + 1];
+//				
+//			}
+//
+//		}
+//
+//
+//		// add first point to close polygon
+//		coordinatesNewList.add(coordinatesNewList.get(0));
+//		Coordinate[] coordinatesNewListArray = coordinatesNewList.toArray(new Coordinate[coordinatesNewList.size()]);
+
+		MultiLineString newLis = gf.createMultiLineString(lineStringsListToUse.toArray(new LineString[lineStringsListToUse.size()]));
+		System.out.println(newLis);
+		CoordinateList ls = new CoordinateList(newLis.getCoordinates()) ;
+		ls.closeRing(); 
+		LinearRing ring = gf.createLinearRing(ls.toCoordinateArray());
+		return ring;
 	}
 
 	private LineString getLineString(LineString exteriorRing, GeometryFactory gf) {
@@ -185,13 +368,19 @@ public class TestGenerateInsertChangelogFile {
 		return createLineString;
 	}
 
+	/**
+	 * The left polygon
+	 * @return
+	 * @throws ParseException
+	 * @throws java.text.ParseException
+	 */
 	private Ar5FlateProvSimpleFeatureEntity addPolygonOne() throws ParseException, java.text.ParseException {
 		WKTReader reader = new WKTReader();
 
 		// with a extra point on shared line
 		//Polygon borderPolygon = (Polygon) reader.read("POLYGON((59.31038099999999957 4.90558199999999989,59.31026539166666822 4.90555743333333449,59.31024421666666768 4.90523724166666675,59.31038099999999957 4.90523000000000042,59.3103815333333344 4.90535530833333322,59.3103815333333344 4.90535530833333322,59.31038099999999957 4.90558199999999989))");
 
-		Polygon borderPolygon = (Polygon) reader.read("POLYGON((59.31038099999999957 4.90558199999999989,59.31026539166666822 4.90555743333333449,59.31024421666666768 4.90523724166666675,59.31038099999999957 4.90523000000000042,59.31038099999999957 4.90558199999999989))");
+		 Polygon borderPolygon = (Polygon) reader.read("POLYGON((59.31038099999999957 4.90558199999999989,59.31026539166666822 4.90555743333333449,59.31024421666666768 4.90523724166666675,59.31038099999999957 4.90523000000000042,59.31038099999999957 4.90558199999999989))");
 		
 		
 		borderPolygon.setSRID(4258);
@@ -226,11 +415,17 @@ public class TestGenerateInsertChangelogFile {
 		return ar5f;
 	}
 
+	/**
+	 * The right polygon
+	 * @return
+	 * @throws ParseException
+	 * @throws java.text.ParseException
+	 */
 	private Ar5FlateProvSimpleFeatureEntity addPolygonTwo() throws ParseException, java.text.ParseException {
 		WKTReader reader = new WKTReader();
 
 		// with a extra point on shared line
-		// Polygon borderPolygon = (Polygon) reader.read("POLYGON((59.31038099999999957 4.90523000000000042,59.31056000000000239 4.90555599999999981,59.31057200000000051 4.90555599999999981,59.31057200000000051 4.90558199999999989,59.31038099999999957 4.90558199999999989,59.3103815333333344 4.90535530833333322,59.31038099999999957 4.90523000000000042,59.31038099999999957 4.90523000000000042))");
+		//Polygon borderPolygon = (Polygon) reader.read("POLYGON((59.31038099999999957 4.90523000000000042,59.31056000000000239 4.90555599999999981,59.31057200000000051 4.90555599999999981,59.31057200000000051 4.90558199999999989,59.31038099999999957 4.90558199999999989,59.3103815333333344 4.90535530833333322,59.31038099999999957 4.90523000000000042,59.31038099999999957 4.90523000000000042))");
 
 		Polygon borderPolygon = (Polygon) reader.read("POLYGON((59.31038099999999957 4.90523000000000042,59.31056000000000239 4.90555599999999981,59.31057200000000051 4.90555599999999981,59.31057200000000051 4.90558199999999989,59.31038099999999957 4.90558199999999989,59.31038099999999957 4.90523000000000042,59.31038099999999957 4.90523000000000042))");
 		
